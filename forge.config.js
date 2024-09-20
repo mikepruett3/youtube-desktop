64902f86d68f3210e7dbd03d1052ec77b1712ff9
Mike Pruett <mikepruett3@gmail.com>
2024-09-20 03:52:52 -0500

    Oops... lets try that again!

--- forge.config.js
+++ forge.config.js
@@ -18,13 +18,6 @@ module.exports = {
       platforms: ['darwin-x64'],
     },
     {
-      name: '@electron-forge/maker-dmg',
-      config: {
-        background: './images/YouTube.png',
-        format: 'ULFO'
-      }
-    },
-    {
       name: '@electron-forge/maker-deb',
       config: {},
     },
